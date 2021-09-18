7766f914b322fad8a53b66c3b4c89a08a6a3d1be
Craig Wilkinson <craig@crwi.uk>
2021-09-18 18:39:52 +0100

    Fix indents in tests

--- chapter-1/bins/test.c
+++ chapter-1/bins/test.c
@@ -10,38 +10,38 @@ unsigned int testCount = 0;
 
 #define ERROR(...) fprintf(stderr, "ERROR: " __VA_ARGS__)
 
-#define TEST_STR(testName, expected, actual)				\
-  testCount++;								\
-  if (!strcmp(actual, expected)) {					\
-    ERROR("Failed test " testName ": expected %s but was %s\n", expected, actual); \
-    failCount++;							\
-  } else {								\
-    printf(".");							\
-  }
+#define TEST_STR(testName, expected, actual)														\
+	testCount++;																													\
+	if (!strcmp(actual, expected)) {																			\
+		ERROR("Failed test " testName ": expected %s but was %s\n", expected, actual); \
+		failCount++;																												\
+	} else {																															\
+		printf(".");																												\
+	}
 
-#define TEST_INT(testName, expected, actual)				\
-  testCount++;								\
-  if (actual != expected) {						\
-    ERROR("Failed test " testName ": expected %d but was %d\n", expected, actual); \
-    failCount++;							\
-  } else {								\
-    printf(".");							\
-  }
-  
+#define TEST_INT(testName, expected, actual)														\
+	testCount++;																													\
+	if (actual != expected) {																							\
+		ERROR("Failed test " testName ": expected %d but was %d\n", expected, actual); \
+		failCount++;																												\
+	} else {																															\
+		printf(".");																												\
+	}
+
 int main() {
-  initTokenFinders();
+	initTokenFinders();
 
-  TokenFinder identifierFinder = makeIdentifierFinder();
+	TokenFinder identifierFinder = makeIdentifierFinder();
 
-  Validity v = validateRange("i32", 0, 3, identifierFinder);
-  TEST_INT("validateRange i32 identifier", VALID, v);
+	Validity v = validateRange("i32", 0, 3, identifierFinder);
+	TEST_INT("validateRange i32 identifier", VALID, v);
 
-  v = validateRange("$myVar", 0, 6, identifierFinder);
-  TEST_INT("validateRange $myVar identifier", VALID, v);
+	v = validateRange("$myVar", 0, 6, identifierFinder);
+	TEST_INT("validateRange $myVar identifier", VALID, v);
 
-  TokenFinder numberFinder = makeNumberFinder();
-  v = validateRange("3.", 0, 2, numberFinder);
-  TEST_INT("validateRange incomplete number", PARTIAL, v);
+	TokenFinder numberFinder = makeNumberFinder();
+	v = validateRange("3.", 0, 2, numberFinder);
+	TEST_INT("validateRange incomplete number", PARTIAL, v);
 
   if (!failCount) {
     printf("\n\033[0;32mPASSED %d TESTS\033[0m\n", testCount);
